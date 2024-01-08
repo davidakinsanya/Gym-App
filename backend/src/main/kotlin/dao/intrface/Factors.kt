05c3f98feab9767076c4c875a86c1d896f8a3312
davidakinsanya <davidakinsanya00@gmail.com>
2024-01-08 20:29:06 +0000

    Including a missing interface to the lasr commit.

---
+++ backend/src/main/kotlin/dao/intrface/Factors.kt
@@ -0,0 +1,19 @@
+package dao.intrface
+
+import dto.StartingELO
+
+interface Factors {
+  
+  fun addBenchFactors(currentELO: Int,
+                      startingELO: StartingELO,
+                      currentWeight: Float): Int
+  
+  fun addSquatFactors(currentELO: Int,
+                      startingELO: StartingELO,
+                      currentWeight: Float): Int
+  
+  fun addDeadLiftFactors(currentELO: Int,
+                         startingELO: StartingELO,
+                         currentWeight: Float): Int
+  
+}
