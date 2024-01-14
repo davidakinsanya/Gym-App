97b8f5b00e9edfe52eba7faeae09a0d904ff4c6c
davidakinsanya <davidakinsanya00@gmail.com>
2024-01-14 15:27:33 +0000

    Adding a springboot project and attaching it to my backend logic so far.

---
+++ microservices/src/main/kotlin/com/uplift/backend/StartingELO/StartingEloApplication.kt
@@ -0,0 +1,11 @@
+package com.uplift.backend.StartingELO
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StartingEloApplication
+
+fun main(args: Array<String>) {
+	runApplication<StartingEloApplication>(*args)
+}
---
+++ microservices/src/test/kotlin/com/uplift/backend/StartingELO/StartingEloApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.uplift.backend.StartingELO
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartingEloApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
