c2b2806fbe5d9e9e83d68b2526cf55d64f430a10
davidakinsanya <davidakinsanya00@gmail.com>
2024-01-22 20:25:07 +0000

    Completing the last bits of documentation for this microservice.

--- starting-elo-microservice/src/main/kotlin/com/uplift/backend/main/StartingEloApplication.kt
+++ starting-elo-microservice/src/main/kotlin/com/uplift/backend/main/StartingEloApplication.kt
@@ -3,9 +3,21 @@ package com.uplift.backend.main
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+/**
+ * This class is the entry point to the microservice.
+ *
+ * @author David
+ */
 @SpringBootApplication
 class StartingEloApplication
 
+/**
+ * This function is the main method tasked with running
+ * the microservice.
+ *
+ * @param args an array of strings representing
+ *             the command line arguments.
+ */
 fun main(args: Array<String>) {
 	runApplication<StartingEloApplication>(*args)
 }
