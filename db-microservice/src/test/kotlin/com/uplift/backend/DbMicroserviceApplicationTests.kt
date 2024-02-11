310bd3edda76e5239eeb2e30cad8447a885402db
davidakinsanya <davidakinsanya00@gmail.com>
2024-02-11 18:10:48 +0000

    Starting the database microservice.

---
+++ db-microservice/src/test/kotlin/com/uplift/backend/DbMicroserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.uplift.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DbMicroserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
