f0f6b0e6c008e17d1360e7f0a426c29817285301
davidakinsanya <davidakinsanya00@gmail.com>
2024-03-01 21:50:35 +0000

    adding a microservice for the premium features.

---
+++ specialty-lift-microservice/src/main/kotlin/com/uplift/backend/main/SpecialtyLiftApplication.kt
@@ -0,0 +1,11 @@
+package com.uplift.backend.main
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpecialtyLiftApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpecialtyLiftApplication>(*args)
+}
