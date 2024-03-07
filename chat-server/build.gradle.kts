c7e5ff19f6f5ccf47847f48892f950640d5e7932
davidakinsanya <davidakinsanya00@gmail.com>
2024-03-07 15:04:09 +0000

    Adding a skeleton project for a chat server backend for the app.

---
+++ chat-server/build.gradle.kts
@@ -0,0 +1,52 @@
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val koin_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.9.22"
+    id("io.ktor.plugin") version "2.3.8"
+    kotlin("plugin.serialization") version "1.9.22"
+}
+
+group = "com.uplift.backend"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-websockets-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-call-logging-jvm")
+    implementation("io.ktor:ktor-server-sessions-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    
+    // Kotlin coroutine dependency
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    // MongoDB Kotlin driver dependency
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:4.10.1")
+
+
+// Koin core features
+    implementation("io.insert-koin:koin-core:$koin_version")
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
+}
+
---
+++ chat-server/src/main/kotlin/com/uplift/backend/main/Application.kt
@@ -0,0 +1,16 @@
+package com.uplift.backend.main
+
+import com.uplift.backend.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSockets()
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity()
+    configureRouting()
+}
