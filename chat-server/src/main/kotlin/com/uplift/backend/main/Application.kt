f002d0585f189c170a5aeaf2f4187714f7d1415a
davidakinsanya <davidakinsanya00@gmail.com>
2024-03-13 02:24:24 +0000

    cleanup

--- chat-server/src/main/kotlin/com/uplift/backend/main/Application.kt
+++ chat-server/src/main/kotlin/com/uplift/backend/main/Application.kt
@@ -1,13 +1,10 @@
 package com.uplift.backend.main
 
-import com.typesafe.config.ConfigFactory
 import com.uplift.backend.di.mainModule
 import com.uplift.backend.plugins.*
-import de.sharpmind.ktor.EnvConfig
 import io.ktor.server.application.*
 import io.ktor.server.config.*
 import org.koin.ktor.plugin.Koin
-import java.io.File
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
